bb947e9d972b5174d939932b3ab1e31d2cdf813f
Lewis <lewis@spellcraft.org>
2025-01-30 04:07:18 -0600

    test: reduce gradient acc

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,6 +6,6 @@ accelerate launch --config_file configs/zero3.yaml src/open_r1/grpo.py \
     --dataset_name AI-MO/NuminaMath-TIR \
     --max_prompt_length 256 \
     --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 16 \
+    --gradient_accumulation_steps 4 \
     --logging_steps 10 \
     --bf16
