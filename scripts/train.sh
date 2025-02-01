262617929cc857fbbe8904b29d652f67213afb3c
Lewis <lewis@spellcraft.org>
2025-02-01 02:06:27 -0600

    fix: training script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 source ~/.bashrc
 
-accelerate launch --config_file configs/zero3.yaml src/open_r1/grpo.py \
+accelerate launch --config_file recipes/accelerate_config/zero3.yaml src/open_r1/grpo.py \
     --output_dir DeepSeek-R1-Distill-Qwen-7B-GRPO \
     --model_name_or_path deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
     --dataset_name AI-MO/NuminaMath-TIR \
