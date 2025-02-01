6aa5d43aaeda7320995c4eb5343da24ef175c236
Lewis <lewis@spellcraft.org>
2025-02-01 02:48:26 -0600

    debug: train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@ source ~/.bashrc
 
 echo "Options:: $@"
 
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/grpo.py \
+NCCL_DEBUG=INFO ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/grpo.py \
     --config recipes/qwen/Qwen2.5-1.5B-Instruct/grpo/confg_full.yaml \
     --output_dir DeepSeek-R1-Distill-Qwen-7B-GRPO \
     --model_name_or_path deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
@@ -11,5 +11,4 @@ ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_con
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 4 \
     --logging_steps 10 \
-    --bf16 \
-    "$@"
+    --bf16 
