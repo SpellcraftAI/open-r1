fccc335e2b6d5c0d20f3872c250910c749ab1ae5
Lewis <lewis@spellcraft.org>
2025-02-02 00:28:23 -0600

    chore: num_processes

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,7 +4,7 @@ echo "Options:: $@"
 
 HF_HUB_ENABLE_HF_TRANSFER=1 ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info accelerate launch \
     --config_file recipes/accelerate_configs/zero3.yaml \
-    --num_processes=7 src/open_r1/grpo.py \
+    src/open_r1/grpo.py \
     --config recipes/qwen/Qwen2.5-1.5B-Instruct/grpo/confg_full.yaml
 
 # ACCELERATE_LOG_LEVEL=info accelerate launch \
